04d315341ed3b0dc737e1f5b86c880482c99118c
Kingostyl <emr8770@gmail.com>
2023-05-14 15:04:47 +0700

    add darkmode

--- dist/script.js
+++ dist/script.js
@@ -11,15 +11,6 @@ darkToggle.addEventListener("click", function () {
   }
 });
 
-if (
-  localStorage.theme === "dark" ||
-  (!("theme" in localStorage) &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches)
-) {
-  document.documentElement.classList.add("dark");
-} else {
-  document.documentElement.classList.remove("dark");
-}
 
 window.onscroll = function () {
   menu();
